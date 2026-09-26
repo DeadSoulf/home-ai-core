c0ce2d8de03dd46abe12ea07666914473581aabf
DeadSoulf <fserver51@gmail.com>
2026-09-26 09:21:09 +0300

    Update Web UI expectations for 0.0.22

--- tests/test_web_ui.cpp
+++ tests/test_web_ui.cpp
@@ -477,7 +477,7 @@ int main()
         ) != std::string::npos
         ||
         cameras.find(
-            "Camera Core 0.0.21"
+            "Camera Core 0.0.22"
         ) == std::string::npos
         ||
         cameras.find(
